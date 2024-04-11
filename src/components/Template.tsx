63949eadfbce91e945d62fa85589655dbe48c104
Eriklps <erikdslopes@gmail.com>
2024-04-11 11:51:46 -0300

    Implemented template

---
+++ src/components/Template.tsx
@@ -0,0 +1,38 @@
+interface TemplateProps {
+    children: React.ReactNode
+}
+
+export const Template: React.FC<TemplateProps> = (props: TemplateProps) => {
+    return (
+        <>
+            <Header />
+                <div className="container mx-auto mt-8 px-4">
+                    { props.children }
+                </div>
+            <Footer />
+        </>
+    )
+}
+
+const Header: React.FC = () => {
+    return (
+        <header className="bg-stone-950 text-white py-3">
+            <div className="container mx-auto flex justify-between items-center">
+                <h1 className="text-3xl font-bold">ImageUploader</h1>
+            </div>
+        </header>
+    )
+}
+
+const Footer: React.FC = () => {
+    return (
+        <footer className="bg-stone-950 text-white py-4 mt-8">
+            <div className="container mx-auto text-center flex justify-center gap-1">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                    <path fill-rule="evenodd" d="M14.447 3.026a.75.75 0 0 1 .527.921l-4.5 16.5a.75.75 0 0 1-1.448-.394l4.5-16.5a.75.75 0 0 1 .921-.527ZM16.72 6.22a.75.75 0 0 1 1.06 0l5.25 5.25a.75.75 0 0 1 0 1.06l-5.25 5.25a.75.75 0 1 1-1.06-1.06L21.44 12l-4.72-4.72a.75.75 0 0 1 0-1.06Zm-9.44 0a.75.75 0 0 1 0 1.06L2.56 12l4.72 4.72a.75.75 0 0 1-1.06 1.06L.97 12.53a.75.75 0 0 1 0-1.06l5.25-5.25a.75.75 0 0 1 1.06 0Z" clip-rule="evenodd" />
+                </svg>
+                Developed by Eriklps
+            </div>
+        </footer>
+    )
+}
