d60b9cd3b2602a503f6fcb409674ef6a23bf3b22
Eriklps <erikdslopes@gmail.com>
2024-04-12 08:24:12 -0300

    Implemente image render on gallery

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,15 +1,41 @@
+'use client'
+
 import { Template, ImageCard } from '@/components';
+import { Image } from '@/resources/image/image.resource';
+import { useImageService } from '@/resources/image/image.service';
+import { useState } from 'react';
 
 export default function GalleryPage(){
+
+    const useService = useImageService();
+    const [images, setImages] = useState<Image[]>([]);
+
+    async function searchImages() {
+        const result = await useService.search();
+        setImages(result);
+        console.table(result);
+    }
+
+    function renderImageCard(image: Image) {
+        return (
+            <ImageCard name={image.name} 
+                       src={image.url} 
+                       size={image.size}
+                       uploadDate={image.uploadDate}  />
+        )
+    }
+
+    function renderImageCards(){
+        return images.map(renderImageCard)
+    }
+
     return (
         <Template>
+            <button className='bg-gray-500' onClick={searchImages}>Search</button>
             <section className="grid grid-cols-3 gap-4">
-                <ImageCard />
-                <ImageCard />
-                <ImageCard />
-                <ImageCard />
-                <ImageCard />
-                <ImageCard />
+                {
+                    renderImageCards()
+                }
             </section>
         </Template>
     )
--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -1,18 +1,18 @@
 interface ImageCardProps {
-    title?: string;
+    name?: string;
     size?: string;
     uploadDate?: string;
     src?: string;
 }
 
 export const ImageCard: React.FC<ImageCardProps> = ({
-    title, size, uploadDate, src
+    name, size, uploadDate, src
 }: ImageCardProps) => {
     return (
         <div className="card relative bg-white rounded-md shadow-md">
             <img src={src} className="h-56 w-full object-cover rounded-t-md" alt="" />
             <div className="card-body p-4">
-                <h5 className="text-x1 font-semibold mb-2 text-gray-600">{title}</h5>
+                <h5 className="text-x1 font-semibold mb-2 text-gray-600">{name}</h5>
                 <p className="text-gray-600">{size}</p>
                 <p className="text-gray-600">{uploadDate}</p>
             </div>
